caa9f4629ab62f4ded0998190e31ed429bb254c3
Vlad <vlad09fedko@gmaul.com>
2026-05-06 17:48:19 +0300

    Improve app

--- src/index.js
+++ src/index.js
@@ -7,10 +7,16 @@ class Animals {
   }
 
   can() {
-    return 'I can';
+    return 'I can'
+  }
+  move() {
+    return `${this.can()} move`
+  }
+  say() {
+    return `${this.can()} say`
   }
   eat() {
-    return `${this.can()} eat`;
+    return `${this.can()} eat`
   }
 
   isAnimal() {
@@ -77,6 +83,10 @@ class Predators extends Mammals {
 class Whales extends Mammals {
   maxAge = 200;
   maxWeight = 200000;
+
+  move() {
+    return 'I can swim'
+  }
 
   isWhale() {
     return this instanceof Whales;
@@ -165,7 +175,7 @@ const shark = new Fish(5, 'Shark', 800, 0);
 const lion = new Predators(8, 'Lion', 190, 4);
 const wolf = new Predators(4, 'Wolf', 40, 4);
 
-const blue_Whale = new Whales(50, 'Blue Whale', 150000, 0);
+const blueWhale = new Whales(50, 'Blue Whale', 150000, 0);
 const orca = new Whales(30, 'Orca', 5000, 0);
 
 const chimpanzee = new Primates(15, 'Chimpanzee', 50, 4);
@@ -182,160 +192,80 @@ const bob = new Human(30, 'Bob', 80, 2);
 
 // Demonstration of objects and their methods
 
-const separator = '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~';
+function showProperties(obj) {
+  const separator =
+    '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~';
+  console.log(separator);
+  console.log(obj);
+  console.log(obj.move());
+  console.log(obj.say());
+  console.log(obj.eat());
+  console.log('isAnimal:', obj.isAnimal());
+  if (obj instanceof Mammals) {
+    console.log('isMammal:', obj.isMammal());
+    if (obj instanceof Predators) {
+      console.log('isPredator:', obj.isPredator());
+      if (obj instanceof Dog) {
+        console.log('isDog:', obj.isDog());
+      }
+    }
+    if (obj instanceof Whales) {
+      console.log('isWhale:', obj.isWhale());
+      if (obj instanceof Dolphin) {
+        console.log('isDolphin:', obj.isDolphin());
+      }
+    }
+    if (obj instanceof Primates) {
+      console.log('isPrimate:', obj.isPrimate());
+      if (obj instanceof Human) {
+        console.log('isHuman:', obj.isHuman());
+      }
+    }
+  }
+  if (obj instanceof Birds) {
+    console.log('isBird:', obj.isBird());
+  }
+  if (obj instanceof Fish) {
+    console.log('isFish:', obj.isFish());
+  }
+}
 
 // Animals
-console.log(separator);
-console.log(genericAnimal);
-console.log('isAnimal:', genericAnimal.isAnimal());
-
-console.log(separator);
-console.log(testCreature);
-console.log('isAnimal:', testCreature.isAnimal());
+showProperties(genericAnimal);
+showProperties(testCreature);
 
 // Mammals
-console.log(separator);
-console.log(elephant);
-console.log('isAnimal:', elephant.isAnimal());
-console.log('isMammal:', elephant.isMammal());
-
-console.log(separator);
-console.log(cat);
-console.log('isAnimal:', cat.isAnimal());
-console.log('isMammal:', cat.isMammal());
+showProperties(elephant);
+showProperties(cat);
 
 // Birds
-console.log(separator);
-console.log(eagle);
-console.log(eagle.move());
-console.log(eagle.say());
-console.log(eagle.eat());
-console.log('isAnimal:', eagle.isAnimal());
-console.log('isBird:', eagle.isBird());
-
-console.log(separator);
-console.log(sparrow);
-console.log(sparrow.move());
-console.log(sparrow.say());
-console.log(sparrow.eat());
-console.log('isAnimal:', sparrow.isAnimal());
-console.log('isBird:', sparrow.isBird());
+showProperties(eagle);
+showProperties(sparrow);
 
 // Fish
-console.log(separator);
-console.log(goldfish);
-console.log(goldfish.move());
-console.log(goldfish.say());
-console.log(goldfish.eat());
-console.log('isAnimal:', goldfish.isAnimal());
-console.log('isFish:', goldfish.isFish());
-
-console.log(separator);
-console.log(shark);
-console.log(shark.move());
-console.log(shark.say());
-console.log(shark.eat());
-console.log('isAnimal:', shark.isAnimal());
-console.log('isFish:', shark.isFish());
+showProperties(goldfish);
+showProperties(shark);
 
 // Predators
-console.log(separator);
-console.log(lion);
-console.log('isAnimal:', lion.isAnimal());
-console.log('isMammal:', lion.isMammal());
-console.log('isPredator:', lion.isPredator());
-
-console.log(separator);
-console.log(wolf);
-console.log('isAnimal:', wolf.isAnimal());
-console.log('isMammal:', wolf.isMammal());
-console.log('isPredator:', wolf.isPredator());
+showProperties(lion);
+showProperties(wolf);
 
 // Whales
-console.log(separator);
-console.log(blue_Whale);
-console.log('isAnimal:', blue_Whale.isAnimal());
-console.log('isMammal:', blue_Whale.isMammal());
-console.log('isWhale:', blue_Whale.isWhale());
-
-console.log(separator);
-console.log(orca);
-console.log('isAnimal:', orca.isAnimal());
-console.log('isMammal:', orca.isMammal());
-console.log('isWhale:', orca.isWhale());
+showProperties(blueWhale);
+showProperties(orca);
 
 // Primates
-console.log(separator);
-console.log(chimpanzee);
-console.log('isAnimal:', chimpanzee.isAnimal());
-console.log('isMammal:', chimpanzee.isMammal());
-console.log('isPrimate:', chimpanzee.isPrimate());
-
-console.log(separator);
-console.log(gorilla);
-console.log('isAnimal:', gorilla.isAnimal());
-console.log('isMammal:', gorilla.isMammal());
-console.log('isPrimate:', gorilla.isPrimate());
+showProperties(chimpanzee);
+showProperties(gorilla);
 
 // Dog
-console.log(separator);
-console.log(buddy);
-console.log(buddy.move());
-console.log(buddy.say());
-console.log(buddy.eat());
-console.log('isAnimal:', buddy.isAnimal());
-console.log('isMammal:', buddy.isMammal());
-console.log('isPredator:', buddy.isPredator());
-console.log('isDog:', buddy.isDog());
-
-console.log(separator);
-console.log(max);
-console.log(max.move());
-console.log(max.say());
-console.log(max.eat());
-console.log('isAnimal:', max.isAnimal());
-console.log('isMammal:', max.isMammal());
-console.log('isPredator:', max.isPredator());
-console.log('isDog:', max.isDog());
+showProperties(buddy);
+showProperties(max);
 
 // Dolphin
-console.log(separator);
-console.log(flipper);
-console.log(flipper.move());
-console.log(flipper.say());
-console.log(flipper.eat());
-console.log('isAnimal:', flipper.isAnimal());
-console.log('isMammal:', flipper.isMammal());
-console.log('isWhale:', flipper.isWhale());
-console.log('isDolphin:', flipper.isDolphin());
-
-console.log(separator);
-console.log(echo);
-console.log(echo.move());
-console.log(echo.say());
-console.log(echo.eat());
-console.log('isAnimal:', echo.isAnimal());
-console.log('isMammal:', echo.isMammal());
-console.log('isWhale:', echo.isWhale());
-console.log('isDolphin:', echo.isDolphin());
+showProperties(flipper);
+showProperties(echo);
 
 // Human
-console.log(separator);
-console.log(alice);
-console.log(alice.move());
-console.log(alice.say());
-console.log(alice.eat());
-console.log('isAnimal:', alice.isAnimal());
-console.log('isMammal:', alice.isMammal());
-console.log('isPrimate:', alice.isPrimate());
-console.log('isHuman:', alice.isHuman());
-
-console.log(separator);
-console.log(bob);
-console.log(bob.move());
-console.log(bob.say());
-console.log(bob.eat());
-console.log('isAnimal:', bob.isAnimal());
-console.log('isMammal:', bob.isMammal());
-console.log('isPrimate:', bob.isPrimate());
-console.log('isHuman:', bob.isHuman());
+showProperties(alice);
+showProperties(bob);
